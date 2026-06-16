2b2b40226c0581c342f987793e9a72fad86eee0d
polloalw <jaw@jaw-m.com>
2026-06-16 17:09:12 -0600

    Replace CotizadorPage content with Quoter component
    cambios para cotizador

--- src/routes/cotizador.tsx
+++ src/routes/cotizador.tsx
@@ -1,28 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Quoter from '../components/Quoter'
 
 export const Route = createFileRoute('/cotizador')({
   component: CotizadorPage,
 })
 
 function CotizadorPage() {
-  return (
-    <div
-      style={{
-        minHeight: '100vh',
-        background: '#07070d',
-        color: 'white',
-        padding: '40px',
-      }}
-    >
-      <h1>Cotizador CCTV del Sur</h1>
-
-      <p>
-        Bienvenido al cotizador en línea.
-      </p>
-
-      <p>
-        Aquí podrás seleccionar equipos y solicitar una cotización personalizada.
-      </p>
-    </div>
-  )
+  return <Quoter />
 }
